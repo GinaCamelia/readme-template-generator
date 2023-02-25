e3ecf8c0b80cf24713283aefc9bf24df47afaca9
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-02-25 15:20:49 +0000

    the function takes in the data object as a parameter and returns a markdown string using template literals.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,34 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Table of contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Test](#test)
+  
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  This application is covered under the ${data.license}.
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Test
+  ${data.test}
+
+  ## Questions
+  If you have any questions, please feel free to ask me at ${data.email}. You can also visit my [GitHub Profile](https://github.com/${data.github}).
 `;
 }
 
